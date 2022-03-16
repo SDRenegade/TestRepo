b3fc99c5aa6c2fa72d72b41f3e72f444272dcdc4
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 20:08:10 -0500

    First commit with files

---
+++ Repo Practice/src/Fruit.java
@@ -0,0 +1,15 @@
+
+public class Fruit {
+	private int serialNumber;
+	
+	public String name;
+	public double cost;
+	
+	Fruit()
+	{
+		name = "Apple";
+		cost = 2.50;
+		serialNumber = 1111;
+		System.out.print("Fruit has been constructed");
+	}
+}
